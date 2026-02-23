e49170c7f7c657f69bd1499e230da0b22b7aa937
kangchanwook <kan0202@naver.com>
2026-02-23 18:22:09 +0900

    [INIT]: CORS 설정 구성

---
+++ src/main/java/seed/seedplusbackend/global/web/CorsConfig.java
@@ -0,0 +1,28 @@
+package seed.seedplusbackend.global.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Value("${cors.allowed-origins}")
+  private String[] allowedOrigins;
+
+  @Value("${cors.allowed-methods}")
+  private String[] allowedMethods;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods(allowedMethods)
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}
