d5ce92bdb87d52b527ffa273e526b8bf7ac6f1bc
kangchanwook <kan0202@naver.com>
2026-04-08 17:53:29 +0900

    [INIT]: Builder 화면 관련 Entity 구현

---
+++ src/main/java/seed/seedplusbackend/domain/entity/builder/BuilderStoreBookmark.java
@@ -0,0 +1,46 @@
+package seed.seedplusbackend.domain.entity.builder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import seed.seedplusbackend.domain.entity.BaseEntity;
+import seed.seedplusbackend.domain.entity.user.StoreOwner;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(
+    name = "builder_store_bookmark",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_builder_store_bookmark_user_store",
+            columnNames = {"user_id", "builder_store_id"}
+        )
+    }
+)
+public class BuilderStoreBookmark extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "bookmarker_id", nullable = false)
+  private StoreOwner bookmarker;
+
+  @Column(name = "builder_store_id", nullable = false)
+  private Long builderStoreId;
+}
---
+++ src/main/java/seed/seedplusbackend/domain/entity/builder/BuilderStoreLike.java
@@ -0,0 +1,50 @@
+package seed.seedplusbackend.domain.entity.builder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import seed.seedplusbackend.domain.entity.BaseEntity;
+import seed.seedplusbackend.domain.entity.user.StoreOwner;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(
+    name = "builder_store_like",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_builder_store_like_user_store",
+            columnNames = {"user_id", "builder_store_id"}
+        )
+    }
+)
+public class BuilderStoreLike extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "builder_store_like_id")
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "liker_id", nullable = false)
+  private StoreOwner liker;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "builder_store_id", nullable = false)
+  private BuilderStore builderStore;
+}
