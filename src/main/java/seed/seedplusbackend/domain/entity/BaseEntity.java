9bbc49c38e05d18ab3ee92bd3a5bdda4c577eead
kangchanwook <kan0202@naver.com>
2026-04-08 17:50:26 +0900

    [INIT]: BaseEntity 추가 및 EnableJPAAuditing 어노테이션 추가

---
+++ src/main/java/seed/seedplusbackend/domain/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package seed.seedplusbackend.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
